226e21bff3799a53bec53cba49e2a7bfda3e04ec
Momkn Development <repo@momkn.com.eg>
2021-07-06 15:13:44 +0200

    create accounts controller

---
+++ SourceOfFund.API/Controllers/AccountsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SourceOfFund.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SourceOfFund.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountBalanceService _accountBalanceService;
+        public AccountsController(IAccountBalanceService accountBalanceService)
+        {
+            _accountBalanceService = accountBalanceService;
+        }
+
+    }
+}
