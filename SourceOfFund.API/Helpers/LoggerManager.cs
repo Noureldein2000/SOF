0f7aa22daee43380cb7724ce96bb54ba916e25ef
DESKTOP-L4MCMGM\Ebram <ebram.mamdouh>
2021-12-05 02:23:15 +0200

    temp fix for logs

---
+++ SourceOfFund.API/Helpers/LoggerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NLog;
+
+namespace SourceOfFund.API.Helpers
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        public void LogError(Exception ex, string message)
+        {
+            logger.Error(ex, message);
+        }
+        public void LogInformation(string message)
+        {
+            logger.Info(message);
+        }
+        public void LogWarning(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
