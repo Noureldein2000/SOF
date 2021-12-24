42b0d420a57be5be5c765a13b5be3645659c6f00
Ebram Mamdoh <ebram.mamdouh>
2021-12-24 02:32:19 +0200

    allow balance before to null

--- SourceOfFund.Data/Entities/BalanceHistory.cs
+++ SourceOfFund.Data/Entities/BalanceHistory.cs
@@ -7,7 +7,7 @@ namespace SourceOfFund.Data.Entities
     public class BalanceHistory : BaseEntity<int>
     {
         public int TransactionID { get; set; }
-        public decimal BalanceBefore { get; set; }
+        public decimal? BalanceBefore { get; set; }
         public int AccountID { get; set; }
         public int BalanceTypeID { get; set; }
         public virtual BalanceType BalanceType { get; set; }
---
+++ SourceOfFund.Data/Migrations/20211224002908_AllowBalanceBeforeNull.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SourceOfFund.Data.Migrations
+{
+    public partial class AllowBalanceBeforeNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BalanceBefore",
+                table: "BalanceHistories",
+                type: "decimal(18,3)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BalanceBefore",
+                table: "BalanceHistories",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)",
+                oldNullable: true);
+        }
+    }
+}
